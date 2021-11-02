c805b52e0ba28b0e6a4a4d03048d26bec422a550
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-02 11:00:17 +0700

    logo: try fix reply by image

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -5,10 +5,47 @@ import os
 import random
 
 from PIL import Image, ImageDraw, ImageFont
+from telethon.tl import types
+from telethon.utils import get_display_name
 from telethon.tl.types import InputMessagesFilterPhotos
 
 from userbot.events import register
 from userbot import CMD_HELP
+
+
+def mediainfo(media):
+    xx = str((str(media)).split("(", maxsplit=1)[0])
+    m = ""
+    if xx == "MessageMediaDocument":
+        mim = media.document.mime_type
+        if mim == "application/x-tgsticker":
+            m = "sticker animated"
+        elif "image" in mim:
+            if mim == "image/webp":
+                m = "sticker"
+            elif mim == "image/gif":
+                m = "gif as doc"
+            else:
+                m = "pic as doc"
+        elif "video" in mim:
+            if "DocumentAttributeAnimated" in str(media):
+                m = "gif"
+            elif "DocumentAttributeVideo" in str(media):
+                i = str(media.document.attributes[0])
+                if "supports_streaming=True" in i:
+                    m = "video"
+                m = "video as doc"
+            else:
+                m = "video"
+        elif "audio" in mim:
+            m = "audio"
+        else:
+            m = "document"
+    elif xx == "MessageMediaPhoto":
+        m = "pic"
+    elif xx == "MessageMediaWebPage":
+        m = "web"
+    return m
 
 
 @register(outgoing=True, pattern="^.logo (.*)")
@@ -31,7 +68,7 @@ async def logo_gen(event):
     else:
         pics = []
         async for i in event.client.iter_messages(
-            "@UltroidLogos", filter=InputMessagesFilterPhotos
+            "@tedelogos", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
@@ -41,7 +78,7 @@ async def logo_gen(event):
     if not bg_:
         pics = []
         async for i in event.client.iter_messages(
-            "@UltroidLogos", filter=InputMessagesFilterPhotos
+            "@tedelogos", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
